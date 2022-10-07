28c5456313ae7277f61e4df2c18a07ff039bddd5
Марков Андрей <Andrejmarko@yandex.ru>
2022-10-07 20:20:42 +0300

    Добавлено задание 6.2.8

---
+++ Task6.2.8/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Task6_2_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        
+        /// <summary>
+        /// Прямоугольник
+        /// </summary>
+        class Rectangle
+        {
+            public int a, b;
+
+            public Rectangle()
+            {
+                a = 6; 
+                b = 4;
+            }
+
+            public Rectangle(int _a)
+            {
+                a = _a;
+                b = _a;
+            }
+
+            public Rectangle(int _a, int _b)
+            {
+                a = _a;
+                b = _b;
+            }
+
+            /// <summary>
+            /// Площать прямоугольника
+            /// </summary>
+            /// <returns></returns>
+            public int Square()
+            {
+                return a * b;
+            }
+        }
+    }
+}
