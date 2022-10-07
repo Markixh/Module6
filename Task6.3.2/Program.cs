c0c4191a1b1524e91d0fd2ae7d3826ee6ea8d801
Марков Андрей <Andrejmarko@yandex.ru>
2022-10-07 21:15:33 +0300

    Добавлено задание 6.3.2

---
+++ Task6.3.2/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task6_3_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bus bus= new Bus();
+            bus.Load = 10;
+            bus.PrintStatus();
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Автобус
+        /// </summary>
+        class Bus
+        {
+            public int? Load;
+
+            public void PrintStatus()
+            {
+                if (Load.HasValue)
+                    Console.WriteLine($"Автобус имеет {Load} мест");
+                else
+                    Console.WriteLine("В Автобусе нет мест");
+            }
+        }
+    }
+}
