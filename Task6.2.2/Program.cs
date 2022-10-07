f2b03889a80b6f82de4bad9226645654f371d818
Марков Андрей <Andrejmarko@yandex.ru>
2022-10-07 18:27:27 +0300

    Добавлено задание 6.2.2

---
+++ Task6.2.2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task6_2_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        class Pen
+        {
+            public string color;
+            public int cost;
+
+            public Pen()
+            {
+                color = "Черный";
+                cost = 100;
+            }
+
+            public Pen(string penColor, int penCost)
+            {
+                color = penColor;
+                cost = penCost;
+            }
+        }
+    }
+}
