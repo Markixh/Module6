e2aa64a713611226c52491361940b46f06a867d5
Андрей Марков <Andrejmarko@yandex.ru>
2022-10-08 22:14:53 +0300

    Добавлено задание 6.5.2

---
+++ Task6.5.2/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Task6_5_2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }    
+
+    class Rectangle
+    {
+        public int a, b;
+        public int Square()
+        {
+            return 0;
+        }
+        public int Perimeter()
+        {
+            return 0;
+        }
+    }
+
+    class Circle
+    {
+        public int r;
+        public double Square()
+        {
+            return 0.00;
+        }
+        public double Сircumference()
+        {
+            return 0;
+        }        
+    }
+
+    class Triangle
+    {
+        public int a, b, c;
+        public double Square()
+        {
+            return 0.00;
+        }
+        public double Сircumference()
+        {
+            return 0.00;
+        }   
+    }    
+}
