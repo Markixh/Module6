ff2a4d36416e709dd31a1625b227513525f753c7
Марков Андрей <Andrejmarko@yandex.ru>
2022-10-09 16:33:36 +0300

    Добавлено задание 6.6.1

---
+++ Task6.6.1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task6_6_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        class TrafficLight
+        {
+            private string Color;
+            
+            private void ChangeColor(string color)
+            {
+            }
+
+            public string GetColor()
+            {
+                return Color;
+            }
+        }
+    }
+}
